1110d4d1995ef41c937cd7da7e9e37b707394eac
DENIS NAUGHTON <mjolinor75@hotmail.comssh-keygen -t rsa -b 4096 -C mjolinor75@hotmail.com>
2018-11-03 19:10:03 -0400

    Final commit

--- main.rb
+++ main.rb
@@ -24,3 +24,21 @@ end
 post '/form' do
 	"You said '#{params[:message]}"
 end
+
+get '/secret' do
+	erb :secret
+end
+
+post '/secret' do
+	params[:secret].reverse
+end
+
+get '/decrypt/:secret' do
+	params[:secret].reverse
+end
+
+not_found do
+	status 404
+	'not found'
+end
+
